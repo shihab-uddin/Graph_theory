acde207c1f8c3a905f15323924ff1cb15afde392
Shihab <tomdshihab@gmail.com>
2020-04-08 10:18:41 +0600

    Dijkstra Algorithm

--- Dijkstra.cpp
+++ Dijkstra.cpp
@@ -1,48 +1,41 @@
-/**
-----> Algorithm : Dijkstra
-----> TIME COMPLEXITY : O(V + ElogV)
-*/
 #include<bits/stdc++.h>
-#define PAIR pair<int, int>
-#define INF 1000000
+#define PAIR pair<unsigned long long int , unsigned long long int>
+#define INF 1000000000000000000
+typedef unsigned long long ULL;
 using namespace std;
 
-int totalNode, node1, node2, weight, src;
+ULL totalNode, node1, node2, weight, src;
 vector<vector<PAIR > > adjList(10000); /*Declaring Adjacency List*/
 priority_queue<PAIR > pq; /*Declaring Priority Queue*/
 
-vector<int> parent;
-vector<int> dist;
+vector<ULL> parent;
+vector<ULL> dist;
 
-int pathFind(int dst){
-    if(dst != src && parent[dst] == -1){ /* if it is not possible to reach from source to destination */
-        cout<<"Path Not Found"<<endl;
-        return 0;
-    }
+void pathFind(ULL dst){
     if(dst == src){ /* Base case : destination == source */
         cout<<src;
-        return 0;
+        return;
     }
     pathFind(parent[dst]);
     cout<<" "<<dst;
 }
 
-void dijkstra(int source){
+void dijkstra(ULL source){
     dist[source] = 0; /*Distance of source = 0*/
     pq.push(make_pair(-0, -source));/*Greatest value has highest priority in PQ, So Put (-) to manage it*/
 
     while(!pq.empty()){
-        pair<int, int> frnt;
+        PAIR frnt; /*Since front of PQ is pair type.*/
         frnt = pq.top(); /*Taking the first element of PQ*/
         pq.pop();
-        int distance, u;
+        ULL distance, u;
         distance = -frnt.first, u = -frnt.second; /*To make those original multiply by (-)*/
 
         if(distance > dist[u]){ /*Since current distance is greater then needn't explore, delete or pop it*/
             continue;
         }
 
-        for(int i = 0; i < adjList[u].size(); i++){
+        for(ULL i = 0; i < adjList[u].size(); i++){
             PAIR v = adjList[u][i]; /*Here second element = distance of u to i && First element = node2*/
             if(dist[u] + v.second < dist[v.first]){/*Relaxation condition :
                                                     distance of u + distance of u to v < distance of v then update */
@@ -55,6 +48,7 @@ void dijkstra(int source){
 }
 
 int main(){
+    ULL destination;
 
     ifstream input("input_dijkstra.txt");
     if(!input.is_open()){
@@ -73,19 +67,29 @@ int main(){
     cin>>src;
     dijkstra(src);
 
-    int destination;
     cout<<"Enter destination : ";
     cin>>destination;
-    cout<<"Cost : "<<dist[destination]<<endl;
-    pathFind(destination);
+
+    if(dist[destination] == INF){ /*Destination is still infinity means not possible to reach there*/
+        cout<<"Path not Found";
+    }else{
+        cout<<"Cost : "<<dist[destination]<<endl;
+        cout<<"Shortest Path : ";
+        pathFind(destination);
+    }
 
     return 0;
 }
 
+/**
+----> Algorithm : Dijkstra
+----> TIME COMPLEXITY : O(V + ElogV)
+*/
+
 /*Input :
 
-5
-0 4 1
+5 ----->Total Vertex.
+0 4 1-->n1, n2, weight.
 1 3 3
 1 4 6
 2 0 6
@@ -100,6 +104,6 @@ int main(){
 Enter Source node : 2
 Enter destination : 3
 Cost : 5
-2 1 3
+Shortest Path : 2 1 3
 
 */
